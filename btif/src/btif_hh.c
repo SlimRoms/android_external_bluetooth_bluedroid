c5d70c12f50fe509ffb6d2ab21e2bfc554641740
Hemant Gupta <hemantg@codeaurora.org>
2013-10-28 11:58:47 +0530

    Bluetooth: HID: Use dynamic memory instead while sending data
    This patch uses dynamic memory allocation while sending HID output data to
    remote device depending on size of data to be sent. Without this patch
    fixed size static buffer of 200 bytes was being used for sending data to
    remote device, which was resulting in crash in case data size to be sent
    to remote device was greater than 200 bytes.
    
    Change-Id: I5ed12422355e88d7ef0391668800d7619ca3b685
    CRs-Fixed: 566444

--- btif/src/btif_hh.c
+++ btif/src/btif_hh.c
@@ -1818,16 +1818,22 @@ static bt_status_t send_data (bt_bdaddr_t *bd_addr, char* data)
 
     else {
         int    hex_bytes_filled;
-        UINT8  hexbuf[200];
+        UINT8  *hexbuf;
         UINT16 len = (strlen(data) + 1) / 2;
 
         if (p_dev->p_buf != NULL) {
             GKI_freebuf(p_dev->p_buf);
             p_dev->p_buf = NULL;
         }
+        hexbuf = GKI_getbuf((UINT16) (strlen(data)));
+        if (hexbuf == NULL) {
+            BTIF_TRACE_ERROR2("%s: Error, failed to allocate RPT buffer, len = %d", __FUNCTION__, strlen(data));
+            return BT_STATUS_FAIL;
+        }
         p_dev->p_buf = GKI_getbuf((UINT16) (len + BTA_HH_MIN_OFFSET + sizeof(BT_HDR)));
         if (p_dev->p_buf == NULL) {
             BTIF_TRACE_ERROR2("%s: Error, failed to allocate RPT buffer, len = %d", __FUNCTION__, len);
+            GKI_freebuf(hexbuf);
             return BT_STATUS_FAIL;
         }
 
@@ -1835,7 +1841,7 @@ static bt_status_t send_data (bt_bdaddr_t *bd_addr, char* data)
         p_dev->p_buf->offset = BTA_HH_MIN_OFFSET;
 
         /* Build a SetReport data buffer */
-        memset(hexbuf, 0, 200);
+        memset(hexbuf, 0, strlen(data));
         hex_bytes_filled = ascii_2_hex(data, len, hexbuf);
         BTIF_TRACE_ERROR2("Hex bytes filled, hex value: %d, %d", hex_bytes_filled, len);
 
@@ -1845,9 +1851,10 @@ static bt_status_t send_data (bt_bdaddr_t *bd_addr, char* data)
             memcpy(pbuf_data, hexbuf, hex_bytes_filled);
             p_dev->p_buf->layer_specific = BTA_HH_RPTT_OUTPUT;
             BTA_HhSendData(p_dev->dev_handle, *bda, p_dev->p_buf);
+            GKI_freebuf(hexbuf);
             return BT_STATUS_SUCCESS;
         }
-
+        GKI_freebuf(hexbuf);
     }
     return BT_STATUS_FAIL;
 }
