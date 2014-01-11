ec3a56cbb3f1b9b8f652a043f61aa4b3f822580a
Satish kumar sugasi <ssugas@codeaurora.org>
2014-01-11 13:26:14 -0800

    Bluetooth: Increase gatt discover service timeout
    this increases discovery timeout to 30secs to avoid gatt
    close due to gatt responses delayed
    
    Change-Id: Iefaedbdd1c255a88a4aa551e2084037d50ba6c20
    CRs-Fixed: 594109

--- bta/dm/bta_dm_act.c
+++ bta/dm/bta_dm_act.c
@@ -5199,7 +5199,7 @@ void bta_dm_ble_broadcast (tBTA_DM_MSG *p_data)
 
 #if ((defined BTA_GATT_INCLUDED) &&  (BTA_GATT_INCLUDED == TRUE))
 #ifndef BTA_DM_GATT_CLOSE_DELAY_TOUT
-#define BTA_DM_GATT_CLOSE_DELAY_TOUT    3000
+#define BTA_DM_GATT_CLOSE_DELAY_TOUT    30000
 #endif
 
 /*******************************************************************************
